59deafc1ebd67bb7c921a9412b476bbd062523b1
maxwelldu <dcj3sjt@126.com>
2017-11-24 15:15:50 +0800

    allow cors

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let cors = require('cors')
 require('./db');
 let users = require('./routes/users');
 let goods = require('./routes/goods')
 let app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
